5050404e32db2336d446f626218fdf64f20a80d2
vanduc2514 <vanduc2514@gmail.com>
2020-02-23 05:19:20 +0700

    First commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function areaRTriangle() {
+    let a = prompt("Xin nhập độ dài một cạnh", "cm");
+    let b = prompt("Xin nhập độ dài cạnh còn lại", "cm");
+    let area = a * b * 0.5;
+    alert("Diện tích hình tam giác vuông là: " + area + "cm2");
+}
